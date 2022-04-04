9de0ad5e7cd45d8c4df6cafd16986f8607d66170
NShan9297 <99927324+NShan9297@users.noreply.github.com>
2022-04-04 14:08:34 -0400

    updated my VS Code

--- PyPoll.py
+++ PyPoll.py
@@ -8,10 +8,11 @@
 # Assign a variable for the file to load and the path.
 from isort import file
 
+# Add our dependencies
 import csv
 import os 
 
-# assign a variable for the file to load and the path.
+# Assign a variable for the file to load and the path.
 file_to_load=os.path.join("Resources", "election_results.csv")
 # Create a filename variable to a direct or indirect path to the file.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
@@ -20,14 +21,11 @@ file_to_save = os.path.join("analysis", "election_analysis.txt")
 with open (file_to_load) as election_data:
     # to do: perform analysis
     print(election_data)
-    
-
-    # to do read and analyze the data here
 
     # read the file object with the reader function.
     file_reader = csv.reader(election_data)
 
-    # Print the header row.
+    # Read and print the header row.
     headers = next(file_reader)
     print(headers)
 
